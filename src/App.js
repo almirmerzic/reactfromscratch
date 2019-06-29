beaaee2b5fca0520e1d22b9bf743dadcf3a4cc5d
almirmerzic <33768032+almirmerzic@users.noreply.github.com>
2019-06-29 20:21:45 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+   render() {
+      return (
+         <div class="container">
+            <div>
+
+               <div class="jumbotron">
+
+                  <h1 class="display-4">Amazing React, Babel, Bootstrap and Webpack</h1>
+
+                  <p class="lead">Created with love</p>
+
+                  <hr class="my-4" />
+
+                  <p>It uses utility classes for typography and spacing to space content out
+                  
+                     the larger container.</p>
+
+                  <p class="lead">
+
+                     <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+
+                  </p>
+
+               </div>
+
+            </div>
+         </div>
+      );
+   } 
+}
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+   entry: './src/main.js',
+   output: {
+      path: path.join(__dirname, '/public'),
+      filename: 'index.js'
+   },
+   devServer: {
+      inline: true,
+      port: 3000
+   },
+   module: {
+      rules: [
+         {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+               presets: ['es2015', 'react']
+            }
+         },
+         {
+            test: /\.css$/,
+            use: [ 'style-loader', 'css-loader' ]
+        }
+      ]
+   },
+   plugins:[
+      new HtmlWebpackPlugin({
+         template: './src/index.html'
+      })
+   ]
+}
